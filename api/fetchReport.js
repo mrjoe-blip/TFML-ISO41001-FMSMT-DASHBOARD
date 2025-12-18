2e02ea69fd35b52412eabcf4021bbb1eb0ac9eb3
mrjoe-blip <driverjoseph001@gmail.com>
2025-12-18 04:56:42 +0100

    Update gasUrl to use new endpoint

--- api/fetchReport.js
+++ api/fetchReport.js
@@ -1,6 +1,7 @@
 export default async function handler(req, res) {
   const { id } = req.query;
-  const gasUrl = `https://script.google.com/macros/s/AKfycbx.../exec?id=${id}`;
+  const gasUrl = `
+https://script.google.com/macros/s/AKfycby2_cbbyl2I3hutAgqt6k7D6lS-U3NW0u1v4dyruiTiKqpRCwReq8JhFCnGHkdXjvBu/exec?id=${id}`;
   
   try {
     const response = await fetch(gasUrl);
